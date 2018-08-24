3b8bd60f3a674182b02373d9e92007a553f65a33
John Santhosh <jsanthos@asu.edu>
2018-08-23 17:18:38 -0700

    Login form.

--- DemoApp/app/src/main/java/com/john/demoapp/MainActivity.java
+++ DemoApp/app/src/main/java/com/john/demoapp/MainActivity.java
@@ -2,8 +2,17 @@ package com.john.demoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view){
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Log.i("Info", "Username: " + username.getText().toString() + ", Password: " + password.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
